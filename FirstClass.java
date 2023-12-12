0bb9c26cc7f78905f68f623a08d3fad0b6ac61e4
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2023-12-12 05:10:45 +0300

    Kullanıcı adı ve şifre kontrolü yapan bir uygulama
    daha önceden oluşturulan kullanıcı adı ve şifresini yeni girilenlerle kontrol eder, doğruysa sizi isminizle karşılar, kullanıcı adı doğru şifre yanlışsa, size yeni şifre koyma hakkı verir , 2 si de yanlışsa program kapatılır

---
+++ FirstClass.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class FirstClass {
+    public static void main(String[]args){
+            Scanner input = new Scanner(System.in);
+            String kullaniciAdi = "Anil Basaran";
+            String sifre = "password123";
+
+            System.out.print("Kullanıcı Adınızı Girin: ");
+            String kullaniciAdiGirdi = input.nextLine();
+
+            System.out.print("Şifrenizi Girin: ");
+            String sifreGirdi = input.nextLine();
+
+            if (kullaniciAdi.equals(kullaniciAdiGirdi) && sifre.equals(sifreGirdi)){
+                System.out.println("Hoşgeldiniz, " + kullaniciAdi );
+            }else if(kullaniciAdi.equals(kullaniciAdiGirdi)){
+                System.out.print( kullaniciAdi + " şifrenizi hatalı girdiniz. Şifrenizi sıfırlamak ister misiniz? (E/H): ");
+                String karar = input.nextLine();
+                if(karar.equals("E")){
+                    System.out.println("Yeni şifreniz eski şifrenizle aynı olmamalıdır.");
+                    System.out.print("Yeni şifrenizi girin: ");
+                    String yeniSifre = input.nextLine();
+                    if( yeniSifre != sifre ){
+                        System.out.print("Yeni şifreniz başarılıyla oluşturuldu.");
+                    }else if(yeniSifre == sifre ){
+                        System.out.println("Yeni şifreniz eski şifrenizle aynı olmamalıdır.");
+                        System.out.println("Çıkış yapıldı");
+                    }
+                }else if(karar == "H") {
+                    System.out.println("Çıkış yapıldı");
+                }
+            }else if(sifre != sifreGirdi){
+                System.out.println("Kullanıcı adınızı ve şifrenizi hatalı girdiniz");
+                System.out.println("Çıkış yapıldı");
+            }
+    }
+}
---
+++ out/production/Beyza-ile-github-denemeleri/README.md
@@ -0,0 +1,2 @@
+# Beyza-ile-github-denemeleri
+Patika'daki GitHub eğitimi ve Beyza ile ilk bootcampimizin ilk reposunu oluşturduk.
